20dad55f8da900cd0b4f2bd92a699a480eed20e5
Basil Shubin <basil.shubin@gmail.com>
2016-12-07 13:14:02 +0700

    reimplemented ``FluentPage`` as ``Publishable`` page

---
+++ fluentcms_publisher/pagetypes/fluentpage/page_type_plugins.py
@@ -0,0 +1,17 @@
+from fluent_pages.extensions import page_type_pool
+from fluent_pages.integration.fluent_contents.page_type_plugins import FluentContentsPagePlugin
+
+from .models import FluentPage
+from .admin import FluentPageAdmin
+
+
+@page_type_pool.register
+class FluentPagePlugin(FluentContentsPagePlugin):
+    model = FluentPage
+    model_admin = FluentPageAdmin
+    sort_priority = 10
+
+    def get_render_template(self, request, fluentpage, **kwargs):
+        # Allow subclasses to easily override it by specifying `render_template` after all.
+        # The default, is to use the template_path from the layout object.
+        return self.render_template or fluentpage.layout.template_path
